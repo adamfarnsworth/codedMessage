d0fa990e58987a804379c452267146a6fa84784e
adamfarnsworth@umail.ucsb.edu <atfarnsworth@umail.ucsb.edu>
2018-04-04 11:12:21 -0700

    now has O(n) run time

--- fooBarCodedMessage/fooBarCodedMessage.py
+++ fooBarCodedMessage/fooBarCodedMessage.py
@@ -4,18 +4,27 @@ def answer(l, t):
     key = t
     messageLocation = []
     tempSum = 0
+    endPosition = 0
 
-    for i in range(len(broadcastList)):
-        tempSum = 0
-        for j in range(i, len(broadcastList)):
+    for i in range(len(broadcastList)):     # itterate through whole list
+        if(i > 0):                          # removes checked start from the count
+            tempSum -= broadcastList[i-1]
+        for j in range(endPosition, len(broadcastList)):
             tempSum += broadcastList[j]
-            if(tempSum > key):
+            if(tempSum > key):              # check next number is sum is to large
+                tempSum -= broadcastList[j]
+                endPosition = j
                 break
             
-            if(tempSum == key):
+            if(tempSum == key):             # return answer when found
               messageLocation = [i,j]
               return messageLocation
-    messageLocation = [-1,-1]
+
+            if(j == len(broadcastList) -1): # no message is in this sequence
+                messageLocation = [-1,-1]
+                return messageLocation
+            endPosition = j
+    messageLocation = [-1,-1]               # no message is in this sequence
     return messageLocation
 
 l = [1, 2, 3, 4]
@@ -31,4 +40,4 @@ print (answer(l, t))
 l = [4, 3, 5, 7, 8]
 t = 12
 print (answer(l, t))
-# answer(l,t) = [-1, -1]
+# answer(l,t) = [0, 2]
